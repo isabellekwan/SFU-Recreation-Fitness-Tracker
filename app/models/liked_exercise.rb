65881567686bf93de384f1ca7e5f17e4e9cc29fc
androopy <114705663+androopy@users.noreply.github.com>
2023-11-11 16:07:40 -0800

    new db for liked exercises for user

---
+++ app/models/liked_exercise.rb
@@ -0,0 +1,2 @@
+class LikedExercise < ApplicationRecord
+end
---
+++ test/models/liked_exercise_test.rb
@@ -0,0 +1,7 @@
+require "test_helper"
+
+class LikedExerciseTest < ActiveSupport::TestCase
+  # test "the truth" do
+  #   assert true
+  # end
+end
