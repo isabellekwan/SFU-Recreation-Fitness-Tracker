cd0aadf52aaca8e1f91ed74e7fdc6e0963a517f8
Chloe <chloecocoyip@gmail.com>
2023-11-17 00:12:13 -0800

    contact done

--- db/schema.rb
+++ db/schema.rb
@@ -44,18 +44,6 @@ ActiveRecord::Schema[7.0].define(version: 2023_11_17_001120) do
     t.datetime "updated_at", null: false
   end
 
-  create_table "progress", force: :cascade do |t|
-    t.bigint "user_id"
-    t.bigint "exercise_id"
-    t.date "date"
-    t.float "weight_lifted"
-    t.integer "duration_complete"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
-    t.index ["exercise_id"], name: "index_progress_on_exercise_id"
-    t.index ["user_id"], name: "index_progress_on_user_id"
-  end
-
   create_table "users", force: :cascade do |t|
     t.string "username"
     t.string "password_digest"
@@ -64,32 +52,5 @@ ActiveRecord::Schema[7.0].define(version: 2023_11_17_001120) do
     t.integer "complete_goals_count", default: 0
   end
 
-  create_table "workout_exercises", force: :cascade do |t|
-    t.bigint "workout_id"
-    t.bigint "exercise_id"
-    t.integer "sets"
-    t.integer "reps"
-    t.float "weight"
-    t.string "weight_unit"
-    t.integer "duration"
-    t.index ["exercise_id"], name: "index_workout_exercises_on_exercise_id"
-    t.index ["workout_id"], name: "index_workout_exercises_on_workout_id"
-  end
-
-  create_table "workouts", force: :cascade do |t|
-    t.bigint "user_id"
-    t.string "name"
-    t.date "date"
-    t.text "description"
-    t.datetime "date_created", null: false
-    t.datetime "date_updated", null: false
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
-    t.index ["user_id"], name: "index_workouts_on_user_id"
-  end
-
   add_foreign_key "goals", "users"
-  add_foreign_key "progress", "users"
-  add_foreign_key "workout_exercises", "workouts"
-  add_foreign_key "workouts", "users"
 end
